22f701b5de6483ed5f3f489668dedcdcc64f8b18
michelreisdev <michelreisdev@gmail.com>
2022-06-04 13:47:57 -0300

    V1

---
+++ src/components/header/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Nav } from "./css";
+import { FaHome, FaSignInAlt, FaUserAlt } from "react-icons/fa";
+
+export default function Header(){
+    return (<>
+        <Nav>
+            <a href=""><FaHome size={24}/></a>
+            <a href=""><FaUserAlt size={24}/></a>
+            <a href=""><FaSignInAlt size={24}/></a>
+        </Nav>
+    </>);
+}
